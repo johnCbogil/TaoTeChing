569a10fe30121b222ebefacafb883d7689cd2a2e
John Bogil <johncbogil@gmail.com>
2015-06-20 23:33:36 -0400

    Created pageVC singleton successfully

---
+++ TaoTeChing/TaoTeChing/PageViewController.h
@@ -0,0 +1,13 @@
+//
+//  PageViewController.h
+//  TaoTeChing
+//
+//  Created by John Bogil on 6/20/15.
+//  Copyright (c) 2015 John Bogil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PageViewController : NSObject
+
+@end
---
+++ TaoTeChing/TaoTeChing/PageViewController.m
@@ -0,0 +1,13 @@
+//
+//  PageViewController.m
+//  TaoTeChing
+//
+//  Created by John Bogil on 6/20/15.
+//  Copyright (c) 2015 John Bogil. All rights reserved.
+//
+
+#import "PageViewController.h"
+
+@implementation PageViewController
+
+@end
