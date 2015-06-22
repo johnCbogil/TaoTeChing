269a1cee81d139bc0caba6546844c28d04a3ba2d
John Bogil <johncbogil@gmail.com>
2015-06-22 19:19:27 -0400

    Added tableviews to bookmarks and more

--- TaoTeChing/TabBarViewController.m
+++ TaoTeChing/TabBarViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    self.selectedIndex = 1; 
+    self.selectedIndex = 1;
 }
 
 - (void)didReceiveMemoryWarning {
