cc98a67df42e0e8ead1c74016004268c18cafd5a
John Bogil <johncbogil@gmail.com>
2015-07-13 17:49:39 -0400

    Created the more table view controller

---
+++ TaoTeChing/TaoTeChing/MoreTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoreTableViewController.h
+//  TaoTeChing
+//
+//  Created by John Bogil on 7/13/15.
+//  Copyright (c) 2015 John Bogil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreTableViewController : UITableViewController
+
+@end
