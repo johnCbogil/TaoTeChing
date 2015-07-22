ce214e451eaffc33c0beced4ac17d45b48751486
John Bogil <johncbogil@gmail.com>
2015-07-22 14:56:22 -0400

    Bookmark forwarding now occurs in the correct directon

--- TaoTeChing/TaoTeChing/BookmarksViewController.m
+++ TaoTeChing/TaoTeChing/BookmarksViewController.m
@@ -135,7 +135,17 @@
                              NSArray *viewControllers = @[zeroVC];
                              
                              // Page direction depends on index number
-                             [[PageViewController pageViewController].pageViewController setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionReverse animated:YES completion:nil];
+                             
+                             DataViewController *currentView = [[PageViewController pageViewController].pageViewController.viewControllers objectAtIndex:0];
+                             NSInteger currentIndex = [[ModelController modelController] indexOfViewController:currentView];
+                             if (indexPath.row > currentIndex) {
+
+                             [[PageViewController pageViewController].pageViewController setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionForward animated:YES completion:nil];
+                             }
+                             else{
+                                 [[PageViewController pageViewController].pageViewController setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionReverse animated:YES completion:nil];
+                                 
+                             }
                          }
                      }];
 }
