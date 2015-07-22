260f7e399c86284c2369254de24adf8ad615654c
John Bogil <johncbogil@gmail.com>
2015-07-22 17:14:59 -0400

    font size feedback works properly

--- TaoTeChing/TaoTeChing/FontSizeTableViewController.m
+++ TaoTeChing/TaoTeChing/FontSizeTableViewController.m
@@ -21,18 +21,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    
     self.title = @"Set Font Size";
     
 }
-
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -44,28 +35,21 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     [[SettingsManager sharedManager]updateFontSize:indexPath.row];
-    
-    // add a checkmark to the selected indexpath.row
-    // remove all the other check marks
-    
     self.lastIndexPath = indexPath;
-    
     [tableView reloadData];
-
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    // Return the number of sections.
     return 1;
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    
     return 3;
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+
     
     // Configure the cell...
     if (indexPath.row == 0) {
@@ -77,7 +61,8 @@
         {
             cell.accessoryType = UITableViewCellAccessoryNone;
         }
-        cell.textLabel.text = @"small";
+        cell.textLabel.text = @"Small";
+        cell.textLabel.font = [UIFont fontWithName:@"Avenir" size:18];
     }
     else if(indexPath.row == 1){
         if ([indexPath compare:self.lastIndexPath] == NSOrderedSame)
@@ -88,7 +73,8 @@
         {
             cell.accessoryType = UITableViewCellAccessoryNone;
         }
-        cell.textLabel.text = @"medium";
+        cell.textLabel.text = @"Medium";
+        cell.textLabel.font = [UIFont fontWithName:@"Avenir" size:22];
     }
     else{
         if ([indexPath compare:self.lastIndexPath] == NSOrderedSame)
@@ -99,55 +85,67 @@
         {
             cell.accessoryType = UITableViewCellAccessoryNone;
         }
-        cell.textLabel.text = @"large";
-
+        cell.textLabel.text = @"Large";
+        cell.textLabel.font = [UIFont fontWithName:@"Avenir" size:26];
     }
+    
+    
+    if ([UIFont fontSize]==18 && indexPath.row == 0) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+    else if ([UIFont fontSize]==22  && indexPath.row == 1) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+    else if ([UIFont fontSize]==26   && indexPath.row == 2) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+    
     return cell;
 }
 
 
 /*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
+ // Override to support conditional editing of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
 
 /*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the row from the data source
+ [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
 
 /*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+ }
+ */
 
 /*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
